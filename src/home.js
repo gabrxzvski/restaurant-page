b0ff692e377009027545bb7090623bdb6890db7c
Gabriel Rodrigues <gabrxzvski@gmail.com>
2021-06-08 16:51:49 -0300

    Changed DOM elements function

--- src/home.js
+++ src/home.js
@@ -1,7 +1,5 @@
-const DOM = (() => {
-    const content = document.getElementById('content');
-    const main = document.createElement('main');
-    main.className = 'main';
+const renderHome = () => {
+    const main = document.getElementById('main');
     const homepage = document.createElement('div');
     homepage.className = 'homepage';
     const title = document.createElement('h1');
@@ -9,20 +7,9 @@ const DOM = (() => {
     const paragraph = document.createElement('p');
     paragraph.textContent = "This is the coffee shop, we sell all kinds of coffee, we started selling coffee a very long time ago and we've been keep selling it ever since, go to our Menu tab so you can see all the great variety of coffee that we have ! If you're interested, go to the Contact tab so you can find and contact us !"
 
-    return {
-        content,
-        main,
-        homepage,
-        title,
-        paragraph
-    }
-})();
-
-const renderHome = () => {
-    DOM.content.appendChild(DOM.main);
-    DOM.main.appendChild(DOM.homepage);
-    DOM.homepage.appendChild(DOM.title);
-    DOM.homepage.appendChild(DOM.paragraph);
+    main.appendChild(homepage);
+    homepage.appendChild(title);
+    homepage.appendChild(paragraph);
 };
 
 export { renderHome };
