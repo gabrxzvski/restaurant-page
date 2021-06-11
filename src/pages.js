d1b54a0751943118c5e40360f39a132eddac0f6f
Gabriel Rodrigues <gabrxzvski@gmail.com>
2021-06-11 09:58:49 -0300

    Finished the event listeners and put all changes together

--- src/pages.js
+++ src/pages.js
@@ -1,42 +1,63 @@
-import { renderNavbar } from './navbar';
-import { renderHome } from './home';
-import { renderMenu } from './menu';
-import { renderFooter } from './footer';
-
+import { renderNavbar } from "./navbar";
+import { renderHome } from "./home";
+import { renderMenu } from "./menu";
+import { renderFooter } from "./footer";
+import { renderContact } from "./contact";
 
 const addMain = () => {
-    const main = document.createElement('main');
-    const content = document.getElementById('content');
-    main.id = "main";
-    content.appendChild(main);
+  const main = document.createElement("main");
+  const content = document.getElementById("content");
+  main.id = "main";
+  content.appendChild(main);
 };
 
-const removeAllChildNodes = parent => {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    };
+const removeAllChildNodes = (parent) => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+};
+
+const selectedElement = (element, color) => {
+  element.style.color = color;
 };
 
 const renderContent = () => {
+  renderNavbar();
+  addMain();
+  renderHome();
+  renderFooter();
 
-    renderNavbar();
-    addMain();
+  const main = document.getElementById("main");
+  const homeBtn = document.querySelector(".nav-item1");
+  const homeBtnText = document.querySelector(".nav-item1 a");
+  const menuBtn = document.querySelector(".nav-item2");
+  const menuBtnText = document.querySelector(".nav-item2 a");
+  const contactBtn = document.querySelector(".nav-item3");
+  const contactBtnText = document.querySelector(".nav-item3 a");
+
+  homeBtn.addEventListener("click", () => {
+    removeAllChildNodes(main);
     renderHome();
-    renderFooter();
+    selectedElement(homeBtnText, "#fcb971");
+    selectedElement(menuBtnText, "#c1c1c1");
+    selectedElement(contactBtnText, "#c1c1c1");
+  });
 
-    const main = document.getElementById('main');
-    const homeBtn = document.querySelector('.nav-item1');
-    const menuBtn = document.querySelector('.nav-item2');
+  menuBtn.addEventListener("click", () => {
+    removeAllChildNodes(main);
+    renderMenu();
+    selectedElement(menuBtnText, "#fcb971");
+    selectedElement(homeBtnText, "#c1c1c1");
+    selectedElement(contactBtnText, "#c1c1c1");
+  });
 
-    homeBtn.addEventListener('click', () => {
-        removeAllChildNodes(main);
-        renderHome();
-    });
-
-    menuBtn.addEventListener('click', () => {
-        removeAllChildNodes(main);
-        renderMenu();
-    });
+  contactBtn.addEventListener("click", () => {
+    removeAllChildNodes(main);
+    renderContact();
+    selectedElement(contactBtnText, "#fcb971");
+    selectedElement(menuBtnText, "#c1c1c1");
+    selectedElement(homeBtnText, "#c1c1c1");
+  });
 };
 
 export { renderContent };
